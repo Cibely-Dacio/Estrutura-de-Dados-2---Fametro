d9fa85703a4b60644c42efef4a1cbe9b00199b91
Cibely-Dacio <cibely16dacio@gmail.com>
2025-09-20 18:26:34 -0400

    Create Questao-7.c

---
+++ Lista_Exercicios_1_Ponteiros./Questao-7.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+void inverterString(char *str) {
+    // Remove o '\n' que o fgets pode deixar no final
+    str[strcspn(str, "\n")] = 0;
+
+    int tamanho = strlen(str);
+    char *inicio = str;
+    char *fim = str + tamanho - 1;
+    char temp;
+
+    while (inicio < fim) {
+        temp = *inicio;
+        *inicio = *fim;
+        *fim = temp;
+        inicio++;
+        fim--;
+    }
+}
+
+int main() {
+    char minha_string[100];
+    
+    printf("Digite uma string para inverter: ");
+    fgets(minha_string, sizeof(minha_string), stdin);
+    
+    printf("\nString Original: %s", minha_string);
+    
+    inverterString(minha_string);
+    
+    printf("String Invertida: %s\n", minha_string);
+
+    return 0;
+}
